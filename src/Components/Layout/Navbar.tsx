6040706515c36c2ac4e57958e9cdc32d8d23abb7
abdulrahman-11-afk <abdulrahmanafolabi907@gmail.com>
2026-04-08 12:43:52 +0100

    added logo

--- src/Components/Layout/Navbar.tsx
+++ src/Components/Layout/Navbar.tsx
@@ -1,9 +1,18 @@
+import { Zap } from "lucide-react";
 import Link from "next/link";
 
 export default function Navbar() {
     return (
             <header className="flex items-center fixed z-50 top-0 bg-white w-full left-0 justify-between px-20 h-20 "> 
-                <h2 className="cursor-pointer text-xl">Transport</h2>
+                <Link href="/" className="flex items-center gap-2.5 mb-4">
+              <div className="w-8 h-8 rounded-lg bg-blue-100 flex items-center justify-center">
+                <Zap size={16} className="text-blue-600" />
+              </div>
+
+              <span className="font-bold text-xl text-gray-900">
+                Transit<span className="text-blue-600">Pro</span>
+              </span>
+            </Link>
                  <ul className="flex items-center text-[14px] justify-center gap-6 ml-16 cursor-pointer ">
                     <li className="hover:text-[#1E3A8A]">Home</li>
                     <li className="hover:text-[#1E3A8A]">Why Choose Us</li>
